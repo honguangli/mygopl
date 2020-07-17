3af9329c064792f8873e8ff268d73ad00c8aedc8
LGH <1947501227@qq.com>
2020-07-17 17:18:44 +0800

    update the golang programming language docs and some golang programing

--- ch04/test/struct_test.go
+++ ch04/test/struct_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"fmt"
+	"image/gif"
 	"testing"
 	"time"
 )
@@ -16,7 +17,8 @@ type Employee struct {
 	ManagerID int
 }
 
-func TestUse(t *testing.T) {
+// 结构体使用
+func TestStructUse(t *testing.T) {
 	var dilbert Employee
 	dilbert.Salary -= 5000 //  demoted, for writing too few lines of code
 
@@ -41,3 +43,129 @@ func EmployeeByID(id int) *Employee {
 	}
 	return &e
 }
+
+// 空结构体
+func TestStructEmpty(t *testing.T) {
+	seen := make(map[string]struct{}) // set of strings
+
+	// ...
+	s := "s"
+	if _, ok := seen[s]; !ok {
+		seen[s] = struct{}{}
+		// ...first time seeing s...
+	}
+}
+
+// 结构体字面值语法
+func TestStructLiteral(t *testing.T) {
+	p := Point{1, 2}
+
+	fmt.Printf("%v\n", p)
+
+	var nframes = 1
+
+	anim := gif.GIF{LoopCount: nframes}
+
+	fmt.Printf("%v\n", anim)
+
+	fmt.Println(Scale(Point{1, 2}, 5))
+
+	pp := &Point{1, 2}
+
+	/*
+		pp := new(Point)
+		*pp = Point{1, 2}
+	*/
+
+	fmt.Println(pp)
+}
+
+type Point struct {
+	X, Y int
+}
+
+func Scale(p Point, factor int) Point {
+	return Point{p.X * factor, p.Y * factor}
+}
+
+func Bonus(e *Employee, percent int) int {
+	return e.Salary * percent / 100
+}
+
+func AwardAnnualRaise(e *Employee) {
+	e.Salary = e.Salary * 105 / 100
+}
+
+// 结构体比较
+func TestStructCompare(t *testing.T) {
+	p := Point{1, 2}
+	q := Point{2, 1}
+	fmt.Println(p.X == q.X && p.Y == q.Y) // "false"
+	fmt.Println(p == q)                   // "false"
+
+	type address struct {
+		hostname string
+		port     int
+	}
+
+	hits := make(map[address]int)
+	hits[address{"golang.org", 443}]++
+}
+
+// 结构体成员
+func TestStructMember(t *testing.T) {
+	var w Wheel
+	w.X = 8
+	w.Y = 8
+	w.Radius = 5
+	w.Spokes = 20
+
+	/*	var w Wheel
+		w.Circle.Center.X = 8
+		w.Circle.Center.Y = 8
+		w.Circle.Radius = 5
+		w.Spokes = 20*/
+
+	/*	var w Wheel
+		w.X = 8      // equivalent to w.Circle.Point.X = 8
+		w.Y = 8      // equivalent to w.Circle.Point.Y = 8
+		w.Radius = 5 // equivalent to w.Circle.Radius = 5
+		w.Spokes = 20*/
+
+	w = Wheel{8, 8, 5, 20}                       // compile error: unknown fields
+	w = Wheel{X: 8, Y: 8, Radius: 5, Spokes: 20} // compile error: unknown fields
+}
+
+type Circle struct {
+	X, Y, Radius int
+}
+
+type Wheel struct {
+	X, Y, Radius, Spokes int
+}
+
+// 嵌入成员
+/*type Point struct {
+	X, Y int
+}
+
+type Circle struct {
+	Center Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle Circle
+	Spokes int
+}*/
+
+// 嵌入匿名成员
+/*type Circle struct {
+	Point
+	Radius int
+}
+
+type Wheel struct {
+	Circle
+	Spokes int
+}*/
