614e480dbfc833f96fd94aec76f014dbb64c2a3d
LGH <1947501227@qq.com>
2020-07-22 17:51:19 +0800

    update the golang programming language docs and some golang programing

--- ch05/test/func_test.go
+++ ch05/test/func_test.go
@@ -2,6 +2,8 @@ package test
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
 	"testing"
 )
 
@@ -27,4 +29,26 @@ func first(x int, _ int) int {
 
 func zero(int, int) int {
 	return 0
+}
+
+// CountWordsAndImages does an HTTP GET request for the HTML
+// document url and returns the number of words and images in it.
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	/* ... */
+	return
 }
